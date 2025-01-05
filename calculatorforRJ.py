3b4fed62a135b124c0cc89df76a646d166dca691
froriz556 <alex_fomof@mail.ru>
2025-01-05 23:08:11 +0300

    Vers2

--- calculatorforRJ.py
+++ calculatorforRJ.py
@@ -1,5 +1,30 @@
 print("Введите 1-ое число:")
-a = input()
-print("Выберите операцию.")
+a = int(input())
 print("Введите 2-ое число:")
-b = input()
+b = int(input())
+message = '''
+Выберете математическую операцию:
+
++ : Сложение
+- : Вычитание
+/ : Деление
+* : Умножение
+
+Ваш выбор:
+'''
+operation = input(message)
+if operation == '+':
+    print('Сложение')
+    d = a + b
+elif operation == '-':
+    print('Вычитание')
+    d = a - b
+elif operation == '/':
+    print('Деление')
+    d = a / b
+elif operation == '*':
+    print('Умножение')
+    d = a * b
+else:
+    print('Неизвестная операция')
+print("Результат:", d)
